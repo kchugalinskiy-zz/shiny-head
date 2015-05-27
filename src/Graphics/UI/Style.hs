397f500152416fdd904a2c88e63e828852098ec7
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2015-05-27 14:55:45 +0300

    Program state workflow prototype

--- src/Graphics/UI/Style.hs
+++ src/Graphics/UI/Style.hs
@@ -1,5 +1,6 @@
 module Graphics.UI.Style where
 
+import Graphics.UI.Event
 import Graphics.UI.InterfaceDescription (Id, Class)
 
 -- Usage example: ForId "Id" [Append ForId "Btn" Apply [Background RGB 255 0 0 ]]
@@ -17,6 +18,12 @@ data Color =
 	RGB Int Int Int |
 	CMYK Int  Int Int Int
 
+class ProgramState s where
+	applyFunction :: s -> s
+	applyFunctionIO :: s -> IO (s)
+
 data Property =
+	forall s. ProgramState s => PureEvent EventTransaction (s -> s) |
+	forall s. ProgramState s => DirtyEvent EventTransaction (s -> IO (s)) |
 	Background Color |
 	Foreground Color
